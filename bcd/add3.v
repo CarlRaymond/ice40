118b7ae96fd8a9d7442bbf2e90420c2f4b5a72c9
Carl Raymond <carl@orcharddev.com>
2016-10-17 00:50:56 -0400

    Refining code.

--- bcd/add3.v
+++ bcd/add3.v
@@ -1,21 +1,20 @@
-// Add3 module: if input is 5 or greater, add 3. Otherwise pass through.
+// Add3 module for BCD conversion: if input is 5 or greater, add 3. Otherwise pass through.
 module add3(in, out);
 
 	input [3:0] in;
 	output [3:0] out;
-	reg [3:0] out;
 	always @ (in)
 	case (in)
-		4'b0000: out <= 4'b0000;	// 0 => 0
-		4'b0001: out <= 4'b0001;	// 1 => 1
-		4'b0010: out <= 4'b0010;	// 2 => 2
-		4'b0011: out <= 4'b0011;	// 3 => 3
-		4'b0100: out <= 4'b0100;	// 4 => 4
-		4'b0101: out <= 4'b1000;	// 5 => 8
-		4'b0110: out <= 4'b1001;	// 6 => 9
-		4'b0111: out <= 4'b1010;	// 7 => 10
-		4'b1000: out <= 4'b1011;	// 8 => 11
-		4'b1001: out <= 4'b1100;	// 9 => 12
-		default: out <= 4'b0000;
+		4'b0000: out = 4'b0000;	// 0 => 0
+		4'b0001: out = 4'b0001;	// 1 => 1
+		4'b0010: out = 4'b0010;	// 2 => 2
+		4'b0011: out = 4'b0011;	// 3 => 3
+		4'b0100: out = 4'b0100;	// 4 => 4
+		4'b0101: out = 4'b1000;	// 5 => 8
+		4'b0110: out = 4'b1001;	// 6 => 9
+		4'b0111: out = 4'b1010;	// 7 => 10
+		4'b1000: out = 4'b1011;	// 8 => 11
+		4'b1001: out = 4'b1100;	// 9 => 12
+		default: out = 4'b0000;
 	endcase
 endmodule
--- bcd/top.v
+++ bcd/top.v
@@ -2,53 +2,141 @@
 
 module top (
 	input hwclk,
-	output SEG_A, 
-	output SEG_B,
-	output SEG_C,
-	output SEG_D,
-	output SEG_E,
-	output SEG_F,
-	output SEG_G,
-	output SEG_DP,
-	output STROBE_UNITS,
-	output STROBE_TENS,
-	output STROBE_HUNDREDS);
+	output SEGMENT_A, 
+	output SEGMENT_B,
+	output SEGMENT_C,
+	output SEGMENT_D,
+	output SEGMENT_E,
+	output SEGMENT_F,
+	output SEGMENT_G,
+	output SEGMENT_DP,
+	output ENABLE_UNITS,
+	output ENABLE_TENS,
+	output ENABLE_HUNDREDS);
 	
+	// 8-bit counter and clock. This is the data that is displayed
+	// on the output.
 	reg[7:0] counter = 0;
 	wire countclk;
 
+	// Three BCD digits holding converted counter value
 	wire [3:0] units;
 	wire [3:0] tens;
 	wire [1:0] hundreds;
-	wire [6:0] unitsSegs;
-	wire [6:0] tensSegs;
-	wire [6:0] hundredsSegs;
-	wire unitsRbo;
-	wire tensRbo;
 
+	// Actively displayed digit
+	wire [3:0] activeDigit;
+
+	// 7-segment display output for active digit
+	reg [6:0] segments;
+	wire displayclk;
+
+	// Ripple-blanking tracking bit. When 1 and current digit
+	// is 0, display is blanked.
+	reg blank = 0;
+
+	// Count clock, 1Hz
+	scaler #(.N(12000000)) scaler1 (hwclk, countclk);
+
+	// Display clock, 20KHz
+	scaler #(.N(600)) scaler2 (hwclk, displayclk);
+
+	// 8-bit binary to 3-digit BCD converter 
 	bcd_combinational bcd (counter, units, tens, hundreds);
 
-	sevensegment unitsDigit(units, 0, unitsSegs);
-	sevensegment tensDigit(tens, 1, tensSegs, tensRbo);
-	sevensegment hundredsDigit(hundreds, tensRbo, hundredsSegs);
+	// BCD to 7 segment converter
+	sevensegment display(activeDigit, 0, segments);
 
-	// Count once per second
-	scaler #(.N(12000000)) scaler1 (hwclk, countclk);
+	// Counter implementation
 	always @(posedge countclk) begin
 	  counter = counter + 1;
 	end
 
-	// Temporary
-	assign SEG_A = unitsSegs[0];
-	assign SEG_B = unitsSegs[1];
-	assign SEG_C = unitsSegs[2];
-	assign SEG_D = unitsSegs[3];
-	assign SEG_E = unitsSegs[4];
-	assign SEG_F = unitsSegs[5];
-	assign SEG_G = unitsSegs[6];
-	assign SEG_DP = 0;
+	// Digit enable bits, active high.
+	reg[2:0] enable = 3'b000;
 
-	assign STROBE_UNITS = 0;
-	assign STROBE_TENS = 1;
-	assign STROBE_HUNDREDS = 1;
+	// State definitions for one-hot digit enable
+	parameter[2:0]
+		START = 3'b000,
+		HUNDREDS = 3'b100,
+		TENS = 3'b010,
+		UNITS = 3'b001
+		;
+
+	// One-hot state cycles (HUNDREDS => TENS => UNITS => HUNDREDS) to display the active digit
+	reg[2:0] displayState = START;
+
+	// Digit cycling and blanking
+	always @(displayclk) begin
+		case(displayState)
+			HUNDREDS: begin
+				activeDigit <= hundreds;
+				if (activeDigit == 0 && blank == 1) begin
+					// Blank the digit
+					enable <= 0;
+				end
+				else begin
+					// Show the digit
+					enable <= displayState;
+					// Don't blank subsequent digits
+					blank <= 0;
+				end
+
+				// Move to next digit
+				displayState <= TENS;
+			end
+
+			TENS: begin
+				activeDigit <= tens;
+				if (activeDigit == 0 && blank == 1) begin
+					// Blank the digit
+					enable <= 0;
+				end
+				else begin
+					// Show the digit
+					enable <= displayState;
+					// Don't blank subsequent digits
+					blank <= 0;
+				end
+
+				// Move to next digit
+				displayState <= UNITS;
+			end
+
+			UNITS: begin
+				activeDigit <= units;
+				// Never blank units
+				enable <= displayState;
+
+				// Prepare to blank MSD on next pass
+				blank <= 0;
+
+				// Move to next digit
+				displayState = HUNDREDS;
+			end
+
+			default: begin
+				// START or other state: initialize
+				blank <= 1;
+				enable <= 0;
+				displayState <= HUNDREDS;
+			end
+		endcase
+	end
+
+
+	// Assign outputs
+	assign SEGMENT_A = segments[0];
+	assign SEGMENT_B = segments[1];
+	assign SEGMENT_C = segments[2];
+	assign SEGMENT_D = segments[3];
+	assign SEGMENT_E = segments[4];
+	assign SEGMENT_F = segments[5];
+	assign SEGMENT_G = segments[6];
+	assign SEGMENT_DP = 0;
+
+	assign ENABLE_UNITS = enable[0];
+	assign ENABLE_TENS = enable[1];
+	assign ENABLE_HUNDREDS = enable[2];
+
 endmodule
